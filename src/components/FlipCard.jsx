6a2e385a5c8bd412b52d5379d9e6bcb381ddae8d
Sumit chouhan <sumitkhan.dhana@gmail.com>
2024-01-19 14:32:18 +0530

    flip card new layout

--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -1,52 +1,89 @@
 import React from "react";
-// import { flipCard } from "./common/Helper";
-import babbuMaan from "../assets/images/png/babbu-maan.jpg"
+import babbuMaan from "../assets/images/png/babbu-maan.jpg";
 const FlipCard = () => {
   return (
     <div className="flex justify-center items-center min-h-screen px-3 !my-12">
       <div className="flex flex-wrap justify-center">
-      <div className="card">
-  <div className="content">
-    <div className="front">
-    <img
+        <div className="lg:w-3/12 sm:w-6/12 px-2">
+          <div className="card">
+            <div className="content">
+              <div className="front">
+                <img
                   className="xl:h-[310px] md:h-[340px] h-[310px] w-full object-cover"
                   src={babbuMaan}
                   alt="singer"
                 />
-                <h3 className="text-white font-bold text-2xl pt-2">
-               hover me
-                </h3>
-    </div>
-    <div className="back">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, distinctio adipisci dolores sint soluta maxime! Ad, quas totam quibusdam autem voluptatum, assumenda mollitia aspernatur sint molestiae commodi corporis doloremque libero?
-    </div>
-  </div>
-</div>
-        {/* {flipCard.map((data , index)=>{
-            return(
-        <div key={index}  className="xl:w-3/12 w-full sm:w-9/12 md:w-6/12 px-2 mt-4 flex flex-col items-center">
-          <div class="flip-card min-w-[320px] lg:min-w-[290px] max-w-[400px] sm:max-w-[unset] w-full h-[370px] md:h-[400px] xl:h-[360px]">
-            <div class="flip-card-inner">
-              <div class="flip-card-front">
-                <img
-                  className="xl:h-[310px] md:h-[340px] h-[310px] w-full object-cover"
-                  src={data.babbuMaan}
-                  alt="singer"
-                />
-                <h3 className="text-white font-bold text-2xl pt-2">
-                  {data.FrontHeading}
-                </h3>
+                <h3 className="text-white font-bold text-2xl pt-2">hover me</h3>
               </div>
-              <div class="flip-card-back flex justify-center items-center flex-col">
-                <p className="text-white p-3">
-                 {data.description}
-                </p>
+              <div className="back">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Dignissimos, distinctio adipisci dolores sint soluta maxime! Ad,
+                quas totam quibusdam autem voluptatum, assumenda mollitia
+                aspernatur sint molestiae commodi corporis doloremque libero?
               </div>
             </div>
           </div>
         </div>
-        )
-        })} */}
+        <div className="lg:w-3/12 sm:w-6/12 px-2">
+          <div className="card">
+            <div className="content">
+              <div className="front">
+                <img
+                  className="xl:h-[310px] md:h-[340px] h-[310px] w-full object-cover"
+                  src={babbuMaan}
+                  alt="singer"
+                />
+                <h3 className="text-white font-bold text-2xl pt-2">hover me</h3>
+              </div>
+              <div className="back">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Dignissimos, distinctio adipisci dolores sint soluta maxime! Ad,
+                quas totam quibusdam autem voluptatum, assumenda mollitia
+                aspernatur sint molestiae commodi corporis doloremque libero?
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="lg:w-3/12 sm:w-6/12 px-2">
+          <div className="card">
+            <div className="content">
+              <div className="front">
+                <img
+                  className="xl:h-[310px] md:h-[340px] h-[310px] w-full object-cover"
+                  src={babbuMaan}
+                  alt="singer"
+                />
+                <h3 className="text-white font-bold text-2xl pt-2">hover me</h3>
+              </div>
+              <div className="back">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Dignissimos, distinctio adipisci dolores sint soluta maxime! Ad,
+                quas totam quibusdam autem voluptatum, assumenda mollitia
+                aspernatur sint molestiae commodi corporis doloremque libero?
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="lg:w-3/12 sm:w-6/12 px-2">
+          <div className="card">
+            <div className="content">
+              <div className="front">
+                <img
+                  className="xl:h-[310px] md:h-[340px] h-[310px] w-full object-cover"
+                  src={babbuMaan}
+                  alt="singer"
+                />
+                <h3 className="text-white font-bold text-2xl pt-2">hover me</h3>
+              </div>
+              <div className="back">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Dignissimos, distinctio adipisci dolores sint soluta maxime! Ad,
+                quas totam quibusdam autem voluptatum, assumenda mollitia
+                aspernatur sint molestiae commodi corporis doloremque libero?
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

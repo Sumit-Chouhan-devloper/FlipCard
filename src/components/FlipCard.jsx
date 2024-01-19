9746e74f6a4a325d85cca508ba9343c0f008471d
Sumit chouhan <sumitkhan.dhana@gmail.com>
2024-01-19 14:23:53 +0530

    new card

--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -1,10 +1,28 @@
 import React from "react";
-import { flipCard } from "./common/Helper";
+// import { flipCard } from "./common/Helper";
+import babbuMaan from "../assets/images/png/babbu-maan.jpg"
 const FlipCard = () => {
   return (
     <div className="flex justify-center items-center min-h-screen px-3 !my-12">
       <div className="flex flex-wrap justify-center">
-        {flipCard.map((data , index)=>{
+      <div className="card">
+  <div className="content">
+    <div className="front">
+    <img
+                  className="xl:h-[310px] md:h-[340px] h-[310px] w-full object-cover"
+                  src={babbuMaan}
+                  alt="singer"
+                />
+                <h3 className="text-white font-bold text-2xl pt-2">
+               hover me
+                </h3>
+    </div>
+    <div className="back">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, distinctio adipisci dolores sint soluta maxime! Ad, quas totam quibusdam autem voluptatum, assumenda mollitia aspernatur sint molestiae commodi corporis doloremque libero?
+    </div>
+  </div>
+</div>
+        {/* {flipCard.map((data , index)=>{
             return(
         <div key={index}  className="xl:w-3/12 w-full sm:w-9/12 md:w-6/12 px-2 mt-4 flex flex-col items-center">
           <div class="flip-card min-w-[320px] lg:min-w-[290px] max-w-[400px] sm:max-w-[unset] w-full h-[370px] md:h-[400px] xl:h-[360px]">
@@ -26,8 +44,9 @@ const FlipCard = () => {
               </div>
             </div>
           </div>
-        </div>)
-        })}
+        </div>
+        )
+        })} */}
       </div>
     </div>
   );
